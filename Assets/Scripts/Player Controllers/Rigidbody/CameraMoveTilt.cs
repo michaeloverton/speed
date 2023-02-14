e3270d64b28208605f1819de9f778f0acd5deac4
Michael Brown <m.overtonbrown@gmail.com>
2023-02-13 22:27:13 -0800

    clean up code and increase gravity from -9.81 to -30

--- Assets/Scripts/Player Controllers/Rigidbody/CameraMoveTilt.cs
+++ Assets/Scripts/Player Controllers/Rigidbody/CameraMoveTilt.cs
@@ -6,25 +6,13 @@ using UnityEngine;
 public class CameraMoveTilt : MonoBehaviour
 {
     [SerializeField] Transform orientation;
-    [SerializeField] float startVelocity;
     [SerializeField] float tiltTime;
     [SerializeField] float maxTilt;
     public float currentMoveTilt { get; private set; }
-    Rigidbody rb;
-
-    // Start is called before the first frame update
-    void Start()
-    {
-        rb = GetComponent<Rigidbody>();
-    }
 
     // Update is called once per frame
     void Update()
     {
-        Vector3 localVelocity = orientation.InverseTransformDirection(rb.velocity);
-        // Tilt based on velocity. Causes camera jitter why?
-        // currentMoveTilt = -Utility.Remap(localVelocity.x, 0, 30, 0, maxTilt);
-
         if(Input.GetKey(KeyCode.A))
         {
             currentMoveTilt = Mathf.Lerp(currentMoveTilt, maxTilt, tiltTime * Time.deltaTime);
